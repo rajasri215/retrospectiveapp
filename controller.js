f8599e91e4114eb081af06656d45cc8b9432c993
rajasri215 <rajasrijanakiraman@gmail.com>
2017-11-29 22:19:11 +0530

    updated controller.js file for syntax errors

--- controller.js
+++ controller.js
@@ -1,10 +1,8 @@
 var http = require("http");
 
-http.createServer(function(request, response)
-{
-  response.writeHead(200, {"content-type":"text/plain"});
+http.createServer(function(request, response){
+  response.writeHead(200, {"Content-Type":"text/plain"});
   response.end("Hello World!!");
-}
-).listen(2305);
+}).listen(2305);
 
 console.log("Server is listening at 2305");
