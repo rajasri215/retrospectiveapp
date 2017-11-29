2adcada5329240418f3ae8fc7fd498ae313dd638
Rajasri Janakiraman <rajasrijanakiraman@gmail.com>
2017-11-29 22:16:00 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -2,8 +2,8 @@ var http = require("http");
 
 http.createServer(function(request, response)
 {
-  response.writeHead("content-type":"text/plain");
-  response.end("Hello World");
+  response.writeHead(200, {"content-type":"text/plain"});
+  response.end("Hello World!!");
 }
 ).listen(2305);
 
